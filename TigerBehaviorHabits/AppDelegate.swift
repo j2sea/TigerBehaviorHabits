14853278161bc1d4a8dede525bde92f1a108af9e
龙俊 <longjun@mqjc.com>
2024-10-31 17:23:16 +0800

    Init

---
+++ TigerBehaviorHabits/AppDelegate.swift
@@ -0,0 +1,27 @@
+import UIKit
+import AppsFlyerLib
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        AppsFlyerLib.shared().appleAppID = "1234567890"
+        AppsFlyerLib.shared().appsFlyerDevKey = "1234567890"
+
+        NotificationCenter.default.addObserver(self, selector: NSSelectorFromString("sendLaunch"), name: UIApplication.didBecomeActiveNotification, object: nil)
+
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
+    }
+
+    @objc func sendLaunch() {
+        AppsFlyerLib.shared().start()
+    }
+}
---
+++ TigerBehaviorHabits/HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.swift
+//  TigerBehaviorHabits
+//
+//  Created by Julien Long on 2024/10/31.
+//
+
+import UIKit
+import StoreKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.isHidden = true
+        
+    }
+    
+    @IBAction func Behaviour(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "BehaviourHomeViewController") as! BehaviourHomeViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func Habit(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "HealthHomeViewController") as! HealthHomeViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func Share(_ sender: Any) {
+        let objectsToShare = ["TigerBehaviorHabits"]
+        let activityVController = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+        activityVController.popoverPresentationController?.sourceView = self.view
+        activityVController.popoverPresentationController?.sourceRect = CGRect(x: 100, y: 200, width: 300, height: 300)
+        self.present(activityVController, animated: true, completion: nil)
+    }
+    
+    @IBAction func Rate(_ sender: Any) {
+        SKStoreReviewController.requestReview()
+    }
+
+}
